9a999348dbe3f4cee0a3f6046f28dde297277b26
Jonathan Oexner <jonathan.oexner@alum.wpi.edu>
2022-02-01 08:54:31 -0500

    Really push new test file.

---
+++ tests/CreateInputCell_test.py
@@ -0,0 +1,23 @@
+from random import Random
+
+import tensorflow as tf
+
+from roxene import Organism
+from roxene.genes import CreateInputCell
+from roxene.core import InputCell
+
+SEED = 84592184
+
+class CreateInputCell_test(tf.test.TestCase):
+
+    def test_execute(self):
+        rng = Random(SEED)
+        org = Organism()
+        initial_value = rng.uniform(-1, 1)
+        gene = CreateInputCell(initial_value)
+        self.assertEmpty(org.cells)
+        gene.execute(org)
+        self.assertEqual(len(org.cells), 1)
+        new_cell: InputCell = org.cells[0]
+        self.assertEqual(new_cell.get_output(), initial_value)
+

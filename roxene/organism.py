5777b1095ff4c6327e7aee20e7202d2973233e00
Jonathan Oexner <jonathan.oexner@alum.wpi.edu>
2022-04-23 16:02:30 -0400

    Rename stuff for incremental changes to help clue in git blame for great justice

---
+++ roxene/organism.py
@@ -0,0 +1,30 @@
+from collections import deque
+from typing import Deque
+
+from .cells import Neuron, Cell, InputCell
+
+
+class Organism(object):
+
+    def __init__(self, input_names=[], output_names=[]):
+        self.inputs: dict[str, InputCell] = dict((input_name, InputCell()) for input_name in input_names)
+        self.outputs: dict[str, Neuron] = {}
+        self.unused_output_names = list(output_names)
+        self.cells: Deque[Cell] = deque(self.inputs.values())
+
+    def set_input(self, input_label, input_value):
+        self.inputs[input_label].value = input_value
+
+    def get_output(self, output_label):
+        return self.outputs[output_label].get_output()
+
+    def update(self):
+        for cell in self.cells:
+            if isinstance(cell, Neuron):
+                cell.update()
+
+    def addNeuron(self, neuron: Neuron):
+        self.cells.appendleft(neuron)
+        if self.unused_output_names:
+            new_output_name = self.unused_output_names.pop()
+            self.outputs[new_output_name] = neuron

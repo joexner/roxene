2cc440f353e64eee06779e60fa988a56674e526c
Jonathan Oexner <jonathan.oexner@alum.wpi.edu>
2026-01-27 14:09:49 -0500

    Fix retarget_connect_neurons range calculation

--- src/roxene/mutagens/retarget_connect_neurons.py
+++ src/roxene/mutagens/retarget_connect_neurons.py
@@ -1,3 +1,5 @@
+from math import ceil
+
 from ..genes.connect_neurons import ConnectNeurons
 from ..mutagen import Mutagen
 from ..util import get_rng
@@ -12,14 +14,9 @@ class RetargetConnectNeurons(Mutagen):
         self.severity = severity
 
     def mutate_ConnectNeurons(self, gene: ConnectNeurons) -> ConnectNeurons:
-        # Max delta is severity * current target index, but at least 1
+        # Low severity makes for small delta
         max_delta = max(1, int(self.severity * gene.tx_cell_index))
-        
-        # Generate non-zero delta: choose from [-max_delta, max_delta] excluding 0
-        rng = get_rng()
-        tx_delta = int(rng.integers(1, max_delta + 1))  # 1 to max_delta
-        if rng.random() < 0.5:
-            tx_delta = -tx_delta  # 50% chance to be negative
-        
-        new_tx_index = max(0, gene.tx_cell_index + tx_delta)
+        sign = get_rng().choice([-1, 1])
+        tx_delta = sign * get_rng().integers(1, max_delta + 1)
+        new_tx_index = max(1, gene.tx_cell_index + tx_delta)
         return ConnectNeurons(new_tx_index, gene.rx_port, parent_gene=gene)

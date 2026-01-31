048af09ec1c9f5afc502bc09e5580270ee5169fc
Jonathan Oexner <jonathan.oexner@alum.wpi.edu>
2026-01-31 17:53:13 -0500

    Clean up Chappie Cheaty test

--- tests/AddConnectNeurons_test.py
+++ tests/AddConnectNeurons_test.py
@@ -6,15 +6,12 @@ from roxene.mutagens import AddConnectNeurons
 
 class AddConnectNeurons_test(unittest.TestCase):
 
-    def test_get_new_gene_returns_connect_neurons(self):
-        """
-        Test that get_new_gene() returns a ConnectNeurons with the configured parameters.
-        """
-        mutagen = AddConnectNeurons(0.01, tx_cell_index=3, rx_port=5)
-        parent = CompositeGene(child_genes=[], iterations=1)
-        
-        gene = mutagen.get_new_gene(parent)
-        
-        self.assertIsInstance(gene, ConnectNeurons)
-        self.assertEqual(gene.tx_cell_index, 3)
-        self.assertEqual(gene.rx_port, 5)
+    def test_basic(self):
+        gene = CompositeGene(child_genes=[], iterations=1)
+        tx_cell_index = 3
+        rx_port = 5
+        mutagen = AddConnectNeurons(0.01, tx_cell_index=tx_cell_index, rx_port=rx_port)
+        mutant = mutagen.get_new_gene(gene)
+        self.assertIsInstance(mutant, ConnectNeurons)
+        self.assertEqual(mutant.tx_cell_index, tx_cell_index)
+        self.assertEqual(mutant.rx_port, rx_port)
